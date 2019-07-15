f9d43d05286403ba579f7401bbe63e5a552c03a5
kalsoom sarwar <17221598-014@uog.edu.pk>
2019-07-16 02:59:33 +0500

    no result

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/home', pathMatch:'full'},
-  // {path: 'home', component: HomeComponent},
+   {path: 'home', component: HomeComponent},
   {path: 'add', component:AddnewComponent}, 
   {path: 'employees', component: EmplistComponent},
   {path: 'contact', component: ContactComponent},
